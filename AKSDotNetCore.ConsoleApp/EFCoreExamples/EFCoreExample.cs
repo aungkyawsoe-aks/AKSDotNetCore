323d20674616ddb082416dae907abbc550a5e4b6
aungkyawsoe-aks <vno412527@gmail.com>
2023-12-23 18:53:41 +0630

    [Complete] EFCore CRUD

---
+++ AKSDotNetCore.ConsoleApp/EFCoreExamples/EFCoreExample.cs
@@ -0,0 +1,103 @@
+﻿using AKSDotNetCore.ConsoleApp.Models;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKSDotNetCore.ConsoleApp.EFCoreExamples
+{
+    public class EFCoreExample
+    {
+        private readonly AppDbContext _dbContext = new AppDbContext();
+        public void Run()
+        {
+            //Read();
+            //Edit(1);
+            //Edit(100);
+            //Create("efcore title", "efcore author", "efcore content");
+            Update(13, "EFCore Title", "Test Author", "Test Content");
+            //Delete(17);
+        }
+
+        private void Read()
+        {
+            var lst = _dbContext.Blogs.AsNoTracking().ToList();
+            foreach (BlogDataModel item in lst)
+            {
+                Console.WriteLine(item.Blog_Id);
+                Console.WriteLine(item.Blog_Title);
+                Console.WriteLine(item.Blog_Author);
+                Console.WriteLine(item.Blog_Content);
+                Console.WriteLine("________________________");
+            }
+        }
+
+        private void Edit(int id)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+
+            if (item is null)
+            {
+                Console.WriteLine("No data Found");
+                return;
+            }
+            Console.WriteLine(item.Blog_Id);
+            Console.WriteLine(item.Blog_Title);
+            Console.WriteLine(item.Blog_Author);
+            Console.WriteLine(item.Blog_Content);
+            Console.WriteLine("___________________");
+        }
+
+        private void Create(string title, string author, string content)
+        {
+            BlogDataModel blog = new BlogDataModel()
+            {
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content = content
+            };
+
+            _dbContext.Blogs.Add(blog);
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            Console.WriteLine(message);
+        }
+
+        private void Update(int id, string title, string author, string content)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+
+            if (item is null)
+            {
+                Console.WriteLine("No data Found");
+                return;
+            }
+            item.Blog_Title = title;
+            item.Blog_Author = author;
+            item.Blog_Content = content;
+
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+            Console.WriteLine(message);
+        }
+
+        private void Delete(int id)
+        {
+            BlogDataModel? item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+
+            if (item is null)
+            {
+                Console.WriteLine("No data Found");
+                return;
+            }
+            _dbContext.Blogs.Remove(item);
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Deleting Successful" : "Deleting Failed";
+            Console.WriteLine(message);
+        }
+    }
+}
--- AKSDotNetCore.ConsoleApp/Program.cs
+++ AKSDotNetCore.ConsoleApp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using AKSDotNetCore.ConsoleApp.AdoDotNetCoreExamples;
 using AKSDotNetCore.ConsoleApp.DapperExamples;
+using AKSDotNetCore.ConsoleApp.EFCoreExamples;
 
 Console.WriteLine("Hello, World!");
 
 //AdoDotNetCoreExample adoDotNetExample = new AdoDotNetCoreExample();
 //adoDotNetExample.Run();
 
-DapperExample dapperExample = new DapperExample();
-dapperExample.Run();
+//DapperExample dapperExample = new DapperExample();
+//dapperExample.Run();
+
+EFCoreExample eFCoreExample = new EFCoreExample();
+eFCoreExample.Run();
+
 
 Console.ReadKey();
