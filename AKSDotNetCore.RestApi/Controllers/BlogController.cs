5bbc9b8164ce3ff1b34cac143c671ea37cdf6d61
aungkyawsoe-aks <vno412527@gmail.com>
2023-12-25 21:36:54 +0630

    RestApi

---
+++ AKSDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,75 @@
+﻿using AKSDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKSDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext = new AppDbContext();
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _dbContext.Blogs.ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{pageNo}/{pageSize}")]
+        public IActionResult GetBlogs(int pageNo, int pageSize)
+        {
+            var lst = _dbContext.Blogs
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var rowCount = _dbContext.Blogs.Count();
+            var pageCount = rowCount / pageSize;
+            if (rowCount % pageSize > 0)
+            {
+                pageCount++;
+            }
+            return Ok(new { IsEndofPage = pageNo >= pageCount, pageCount = pageCount, pageSize = pageSize, pageNo = pageNo, Data = lst });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            var item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogDataModel blog)
+        {
+            _dbContext.Blogs.Add(blog);
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Saving Successful" : "Saving Failed";
+            return Ok(message);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlogs()
+        {
+            return Ok("put");
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlogs()
+        {
+            return Ok("patch");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlogs()
+        {
+            return Ok("delete");
+        }
+    }
+}
