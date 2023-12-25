454d4083cc790574bb34735e96b4f36cc980786c
aungkyawsoe-aks <vno412527@gmail.com>
2023-12-26 01:03:00 +0630

    [Complete] BlogAdoDotNetApi Read

--- AKSDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
+++ AKSDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -1,8 +1,10 @@
 ﻿using AKSDotNetCore.RestApi.Models;
+using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
+using System.Reflection;
 using System.Xml;
 
 namespace AKSDotNetCore.RestApi.Controllers
@@ -42,9 +44,44 @@ namespace AKSDotNetCore.RestApi.Controllers
                 Blog_Id = Convert.ToInt32(dr["Blog_Id"]),
                 Blog_Title = Convert.ToString(dr["Blog_Title"]),
                 Blog_Author = Convert.ToString(dr["Blog_Author"]),
-                Blog_Content = Convert.ToString(dr["Blog_Content"]),
+                Blog_Content = Convert.ToString(dr["Blog_Content"])
             }).ToList();
             return Ok(lst);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            string query = @"SELECT [Blog_Id]
+      ,[Blog_Title]
+      ,[Blog_Author]
+      ,[Blog_Content]
+  FROM [dbo].[Tbl_Blog] where Blog_Id = @Blog_Id";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Blog_Id", id);
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                return BadRequest("No data found");
+            }
+
+            BlogDataModel blogItem = new BlogDataModel()
+            {
+                Blog_Id = Convert.ToInt32(dt.Rows[0]["Blog_Id"]),
+                Blog_Title = Convert.ToString(dt.Rows[0]["Blog_Title"]),
+                Blog_Author = Convert.ToString(dt.Rows[0]["Blog_Author"]),
+                Blog_Content = Convert.ToString(dt.Rows[0]["Blog_Content"]),
+            };
+
+            return Ok(blogItem);
         }
     }
 }
