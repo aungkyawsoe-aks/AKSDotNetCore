b9c923c1b80b246e4c99fe122cbf1bb0673ca4ae
aungkyawsoe-aks <vno412527@gmail.com>
2023-12-25 22:16:34 +0630

    [Complete] RestApi

--- AKSDotNetCore.RestApi/Controllers/BlogController.cs
+++ AKSDotNetCore.RestApi/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using AKSDotNetCore.RestApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace AKSDotNetCore.RestApi.Controllers
 {
@@ -55,21 +56,73 @@ namespace AKSDotNetCore.RestApi.Controllers
         }
 
         [HttpPut]
-        public IActionResult UpdateBlogs()
+        public IActionResult UpdateBlogs(int id, BlogDataModel blog)
         {
-            return Ok("put");
+            var item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+            }
+
+            if (string.IsNullOrEmpty(blog.Blog_Title))
+            {
+                return BadRequest("Blog Title is required");
+            }
+            if (string.IsNullOrEmpty(blog.Blog_Author))
+            {
+                return BadRequest("Blog Author is required");
+            }
+            if (string.IsNullOrEmpty(blog.Blog_Content))
+            {
+                return BadRequest("Blog Content is required");
+            }
+
+            item.Blog_Title = blog.Blog_Title;
+            item.Blog_Author = blog.Blog_Author;
+            item.Blog_Content = blog.Blog_Content;
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+            return Ok(message);
         }
 
         [HttpPatch]
-        public IActionResult PatchBlogs()
+        public IActionResult PatchBlogs(int id, BlogDataModel blog)
         {
-            return Ok("patch");
+            var item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+            }
+
+            if (!string.IsNullOrEmpty(blog.Blog_Title))
+            {
+                item.Blog_Title = blog.Blog_Title;
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Author))
+            {
+                item.Blog_Author = blog.Blog_Author;
+            }
+            if (!string.IsNullOrEmpty(blog.Blog_Content))
+            {
+                item.Blog_Content = blog.Blog_Content;
+            }
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Updating Successful" : "Updating Failed";
+            return Ok(message);
         }
 
-        [HttpDelete]
-        public IActionResult DeleteBlogs()
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlogs(int id)
         {
-            return Ok("delete");
+            var item = _dbContext.Blogs.FirstOrDefault(x => x.Blog_Id == id);
+            if (item is null)
+            {
+                return NotFound("No data found");
+            }
+            _dbContext.Blogs.Remove(item);
+            int result = _dbContext.SaveChanges();
+            string message = result > 0 ? "Deleting Successful" : "Deleting Failed";
+            return Ok(message);
         }
     }
 }
--- AKSDotNetCore.RestApi/Models/BlogDataModel.cs
+++ AKSDotNetCore.RestApi/Models/BlogDataModel.cs
@@ -13,9 +13,9 @@ namespace AKSDotNetCore.RestApi.Models
     {
         [Key]
         public int Blog_Id { get; set; }
-        public string Blog_Title { get; set; }
-        public string Blog_Author { get; set; }
-        public string Blog_Content { get; set; }
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
 
     }
 }
