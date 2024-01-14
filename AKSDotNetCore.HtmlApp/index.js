9c621de4c91299dc0ecd9d90d50961e9227bb862
Dell-696 <Dell-696@DESKTOP-075JE7I>
2024-01-15 00:49:15 +0630

    [complete] JQuery, cdn, bootstrap, local storage, session storage

---
+++ AKSDotNetCore.HtmlApp/index.js
@@ -0,0 +1,20 @@
+const tblName = "Tbl_Name";
+$('#btnSave').click(function () {
+    let lst = [];
+    if (localStorage.getItem(tblName) != null) {
+        lst = JSON.parse(localStorage.getItem(tblName));
+    }
+    const name = $('#txtName').val();
+    const data = {
+        Id: uuidv4(),
+        Name: name
+    }
+    lst.push(data);
+    localStorage.setItem(tblName, JSON.stringify(lst));
+})
+
+function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
