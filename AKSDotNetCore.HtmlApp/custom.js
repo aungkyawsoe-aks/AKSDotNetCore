f39a2963189037fb2f4bcde4fa9da3bb87410d2b
Dell-696 <Dell-696@DESKTOP-075JE7I>
2024-01-18 14:16:58 +0630

    [complete] notiflix, datatable, datepicker

--- AKSDotNetCore.HtmlApp/custom.js
+++ AKSDotNetCore.HtmlApp/custom.js
@@ -1,9 +1,15 @@
 function successMessage(message) {
-    Swal.fire({
-        title: "Success",
-        text: message,
-        icon: "success"
-    });
+    // Swal.fire({
+    //     title: "Success",
+    //     text: message,
+    //     icon: "success"
+    // });
+
+    Notiflix.Report.success(
+        'Success',
+        message,
+        'Okay',
+        );
 }
 
 function confirmMessage(message) {
@@ -19,15 +25,20 @@ function confirmMessage(message) {
         });
     });
 
-    // return new Promise(function (myResolve, myReject) {
-    //     Swal.fire({
-    //         title: "Confirm",
-    //         text: message,
-    //         icon: "warning",
-    //         showCancelButton: true,
-    //         confirmButtonText: "Yes"
-    //     }).then((result) => {
-    //         myResolve(result.isConfirmed);
-    //     });
+    // return new Promise(function (myResolve, myReject){
+    //     Notiflix.Confirm.show(
+    //         'Confirm',
+    //         message,
+    //         'Yes',
+    //         'No',
+    //         function okCb() {
+    //             myResolve(true);
+    //         },
+    //         function cancelCb() {
+    //             myReject(false);
+    //         },
+    //         {
+    //         },
+    //     );
     // });
 }
