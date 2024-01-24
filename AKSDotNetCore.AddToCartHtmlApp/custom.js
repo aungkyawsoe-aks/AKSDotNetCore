dab5db44d503fa3a8d96d636c176435f0f92d849
Aung Kyaw Soe <ak.soe@mit.com.mm>
2024-01-24 13:52:40 +0630

    -

--- AKSDotNetCore.AddToCartHtmlApp/custom.js
+++ AKSDotNetCore.AddToCartHtmlApp/custom.js
@@ -1,11 +1,11 @@
-function successMessage(message){
+function successMessage(message) {
     Swal.fire({
         position: "center",
         icon: "success",
         title: message,
         showConfirmButton: false,
         timer: 1500
-      });
+    });
 
     // Notiflix.Report.success(
     //     'Success',
@@ -14,30 +14,20 @@ function successMessage(message){
     //     );
 }
 
-function confirmMessage(message){
+function confirmQuantityChange(product_id, type) {
     return new Promise(function (myResolve, myReject) {
         Swal.fire({
-            title: "Confirm",
-            text: message,
-            icon: "warning",
+            title: `Confirm Quantity ${type.charAt(0).toUpperCase() + type.slice(1)}`,
+            text: `Are you sure you want to ${type} the quantity?`,
+            icon: 'question',
             showCancelButton: true,
-            confirmButtonText: "Yes"
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'Cancel'
         }).then((result) => {
-            myResolve(result.isConfirmed);
+            if (result.value) {
+                changeQuantityCart(product_id, type);
+                Swal.fire(`${type.charAt(0).toUpperCase() + type.slice(1)}d!`, `Quantity has been ${type}ed.`, 'success');
+            }
         });
     });
-
-    // Swal.fire({
-    //     title: `Confirm Quantity ${type.charAt(0).toUpperCase() + type.slice(1)}`,
-    //     text: `Are you sure you want to ${type} the quantity?`,
-    //     icon: 'question',
-    //     showCancelButton: true,
-    //     confirmButtonText: `Yes, ${type} it!`,
-    //     cancelButtonText: 'Cancel'
-    // }).then((result) => {
-    //     if (result.value) {
-    //         changeQuantityCart(product_id, type);
-    //         Swal.fire(`${type.charAt(0).toUpperCase() + type.slice(1)}ed!`, `Quantity has been ${type}ed.`, 'success');
-    //     }
-    // });
 }
