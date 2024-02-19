140499e3280b1fcc374513817047f861c0306272
Aung Kyaw Soe <ak.soe@mit.com.mm>
2024-02-20 02:08:04 +0630

    [complete] Mvc BlogRefit CRUD

--- AKSDotNetCore.MvcApp/Controllers/BlogRefitController.cs
+++ AKSDotNetCore.MvcApp/Controllers/BlogRefitController.cs
@@ -1,23 +1,57 @@
 ﻿using AKSDotNetCore.ConsoleApp.RefitExamples;
 using AKSDotNetCore.MvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Refit;
 
 namespace AKSDotNetCore.MvcApp.Controllers
 {
     public class BlogRefitController : Controller
     {
-        private readonly IBlogApi __blogApi;
+        private readonly IBlogApi _blogApi;
 
         public BlogRefitController(IBlogApi blogApi)
         {
-            __blogApi = blogApi;
+            _blogApi = blogApi;
         }
 
         public async Task<IActionResult> IndexAsync()
         {
             var lst = new List<BlogDataModel>();
-            lst = await __blogApi.GetBlogs();
+            lst = await _blogApi.GetBlogs();
             return View(lst);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = new BlogDataModel();
+            item = await _blogApi.GetBlog(id);
+            return View(item);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Save(BlogDataModel model)
+        {
+            var message = await _blogApi.CreaeBlog(model);
+            await Console.Out.WriteLineAsync(message);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id, BlogDataModel model)
+        {
+            var message = await _blogApi.UpdateBlog(id, model);
+            await Console.Out.WriteLineAsync(message);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var message = await _blogApi.DeleteBlog(id);
+            await Console.Out.WriteLineAsync(message);
+            return RedirectToAction("Index");
         }
     }
 }
