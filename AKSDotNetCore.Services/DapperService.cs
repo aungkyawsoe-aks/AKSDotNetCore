5be1a7f300418842db1366dc98ca66b972c6b50c
Aung Kyaw Soe <ak.soe@mit.com.mm>
2024-03-06 00:35:48 +0630

    [complete] DapperService ConsoleApp

---
+++ AKSDotNetCore.Services/DapperService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace AKSDotNetCore.Services
+{
+    public class DapperService
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+
+        public DapperService(SqlConnectionStringBuilder sqlConnectionStringBuilder)
+        {
+            _sqlConnectionStringBuilder = sqlConnectionStringBuilder;
+        }
+
+        public IEnumerable<T> Query<T>(string sql, object? param = null, CommandType commandType = CommandType.Text)
+        {
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            return db.Query<T>(sql, commandType: commandType);
+        }
+
+        public T QueryFirstOrDefault<T>(string sql, object? param = null, CommandType commandType = CommandType.Text)
+        {
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            return db.Query<T>(sql, commandType: commandType).FirstOrDefault()!;
+        }
+
+        public IEnumerable<dynamic> Query(string sql, object? param = null, CommandType commandType = CommandType.Text)
+        {
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            return db.Query(sql, commandType: commandType);
+        }
+
+        public int Execute(string sql, object? param = null, CommandType commandType = CommandType.Text)
+        {
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(sql, commandType: commandType);
+            return result;
+        }
+    }
+}
